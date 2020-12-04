27eedd93eb2e36f7b4e9c945c167f8ea0d92cabb
Bryce Shearn <bryce_shearn@yahoo.com>
2020-12-04 17:02:43 -0500

    complete Objective2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,8 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Bryce Shearn";
+    int age = 26;
+    String state = "Idaho";
+    System.out.println("Name: " + name + " " + "Age: " +  age + " " + "State: " + state);
+  }
+}
